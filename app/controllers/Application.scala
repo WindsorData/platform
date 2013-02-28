a2ea6b6c324bfbc1f76afd722551feff6975b7a7
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-02-28 13:04:06 -0300

    Adding minimal file upload

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,25 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import input.SpreadsheetLoader
 
+//No content-negotiation yet. Just assume HTML for now
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
+  }
+
+  def newCompany = Action(parse.multipartFormData) { request =>
+    request.body.file("dataset").map { dataset =>
+      Ok("File uploaded at "+ dataset.ref.file.getAbsolutePath)
+    }.getOrElse {
+      Redirect(routes.Application.index).flashing("error" -> "Missing file")
+    }
   }
   
+  def company(id: Long) = TODO
+  
+  def companies = TODO
+
 }
