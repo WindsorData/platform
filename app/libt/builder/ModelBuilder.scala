61785fdf034b047f82bd6bf93dae944722dcd86d
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-04-10 13:29:38 -0300

    Introducing model builders

---
+++ app/libt/builder/ModelBuilder.scala
@@ -0,0 +1,36 @@
+package libt.builder
+import libt._
+import scala.collection.mutable.{ Map => MutableMap }
+import scala.collection.mutable.Buffer
+
+class ModelBuilder {
+  type MModel = MutableMap[Symbol, AnyRef]
+  type MCol = Buffer[AnyRef]
+  val model = MutableMap[Symbol, AnyRef]()
+
+  def +=(modelEntry: (Path, Value[_])) = setWithRoot(model, modelEntry._1, modelEntry._2)
+
+  private def setWithRoot(untypedRoot: AnyRef, path: Path, value: Value[_]): Unit = {
+    val root = untypedRoot.asInstanceOf[MModel]
+    def getOrCreate(m : MModel, k : Symbol, default: AnyRef = MutableMap[Symbol, AnyRef]()) =
+      m.getOrElseUpdate(k, default)
+      
+    path match {
+      case Route(field) :: Nil => root += (field -> value)
+      case Route(field) :: Route(next) :: tail =>
+        setWithRoot(getOrCreate(root, field), Route(next) :: tail, value)
+      case Route(field) :: Index(index) :: Nil =>
+        getOrCreate(root, field).asInstanceOf[MCol](0) = value       
+      case Route(field) :: Index(index) :: tail =>
+        setWithRoot(getOrCreate(root, field, Buffer[AnyRef]()).asInstanceOf[MCol](index), tail, value)
+    }
+  }
+  
+  def build: Model = convert(model).asInstanceOf[Model]
+  
+  def convert(element: AnyRef) : Element = element match {
+    case v:Value[_] => v
+    case m:MModel => Model(m.mapValues(convert(_).asInstanceOf[Element]).toSet)
+    case c:MCol => Col(c.map(convert) : _*)
+  }
+}
--- test/input/TestDriver.scala
+++ test/input/TestDriver.scala
@@ -10,7 +10,6 @@ import org.apache.poi.ss.util.CellUtil
 import org.apache.poi.ss.usermodel.Sheet
 import model._
 import scala.collection.mutable.MapBuilder
-import scala.collection.mutable.{ Map => MutableMap }
 import org.apache.poi.ss.usermodel.Cell
 import com.mongodb.DBObject
 import libt.Value
@@ -20,75 +19,109 @@ import libt.export.spreadsheet.util._
 import libt.export.spreadsheet.ColumnOrientedReader
 import scala.collection.mutable.Buffer
 import libt.TModel
+import libt.TModel
+import libt.TString
+import libt.Element
+import libt.TElement
+import libt.Element
+import libt.Col
+import libt.Path
+import libt.TValue
+import libt.TNumber
+import libt.TEnum
+import libt.Route
+import libt.Index
+import libt.builder.ModelBuilder
 
 @RunWith(classOf[JUnitRunner])
 class TestDriver extends FunSpec {
 
-  type DataItemBuilder = Buffer[(Symbol, Value[_])]
+//  type DataItemBuilder = Buffer[(Symbol, Element)]
   type DataItem = Model
   //  abstract class DataValue
   //  case class SingleValue(input: Value[_]) extends DataValue
   //  case class Values(inputs: Seq[DataItem]) extends DataValue
-
-  trait Column {
-    def read(reader: CellReader, dataItemBuilder: DataItemBuilder)
-  }
-
-  case class Feature[A](fieldName: Symbol, featureType: FeatureType[A]) extends Column {
-    def read(reader: CellReader, dataItemBuilder: DataItemBuilder) =
-      dataItemBuilder += (fieldName -> featureType.read(reader))
-  }
-
-  case object Gap extends Column {
-    override def read(reader: CellReader, dataItemBuilder: DataItemBuilder) =
-      reader.skip(1)
-  }
-
-  case class Mapper(columns: Seq[Column], width: Int = 10, heigth: Int = 10) {
-    def read(sheet: Sheet): Seq[DataItem] =
-      sheet.rows.grouped(6).map { inputGroup =>
-        val dataItemBuilder = Buffer[(Symbol, Value[_])]()
-        val reader = new ColumnOrientedReader(inputGroup)
-        for (column <- columns)
-          column.read(reader, dataItemBuilder)
-        Model()
-      }.toSeq
-  }
-
-  trait FeatureType[A] {
+  
+  implicit def tValue2FeatureReader(tValue : TValue) : FeatureReader[_] = tValue match {
+    case TString => StringReader
+    case TNumber => NumericReader
+    case e : TEnum => EnumReader(e) 
+  } 
+    
+    
+  trait FeatureReader[A] {
     def read(reader: CellReader): Value[A]
     def readWithDefault(reader: CellReader, defaultValue: A): Value[A]
   }
 
-  case object NumericType extends FeatureType[BigDecimal] {
+  case object NumericReader extends FeatureReader[BigDecimal] {
     def read(reader: CellReader) = reader.numeric
     def readWithDefault(reader: CellReader, defaultValue: BigDecimal) = reader.numericWithDefault(defaultValue)
   }
 
-  case object StringType extends FeatureType[String] {
+  case object StringReader extends FeatureReader[String] {
     def read(reader: CellReader) = reader.string
     def readWithDefault(reader: CellReader, defaultValue: String) = reader.stringWithDefault(defaultValue)
   }
-  case class EnumType(values: Traversable[String]) extends FeatureType[String] {
-    //TODO: do validation
-    def validate(input: Value[String]) = {
-      if (input.value.isEmpty || values.exists(_ == input.value.get)) {
-        input
-      } else {
-        throw new IllegalArgumentException
-      }
-    }
+  case class EnumReader(enum: TEnum) extends FeatureReader[String] {
+//    //TODO: do validation
+//    def validate(input: Value[String]) = {
+//      if (input.value.isEmpty || enum.exists(_ == input.value.get)) {
+//        input
+//      } else {
+//        throw new IllegalArgumentException
+//      }
+//    }
 
-    def read(reader: CellReader) = validate(reader.string)
+    def read(reader: CellReader) = reader.string
 
     def readWithDefault(reader: CellReader, defaultValue: String) =
-      validate(reader.stringWithDefault(defaultValue))
+      reader.stringWithDefault(defaultValue)
   }
 
-  case class WithDefault[A](featureType: FeatureType[A], defaultValue: A) extends FeatureType[A] {
+  case class WithDefault[A](featureType: FeatureReader[A], defaultValue: A) extends FeatureReader[A] {
     def read(reader: CellReader) = featureType.readWithDefault(reader, defaultValue)
     def readWithDefault(reader: CellReader, defaultValue: A) = ???
   }
+ 
+  
+  case class Mapping(columns: Column*)
+  
+  trait Column {
+    def read(reader: CellReader, schema : TElement, modelBuilder: ModelBuilder)
+  }
+
+  case class Feature(path: Path) extends Column {
+    def read(reader: CellReader, schema : TElement, modelBuilder: ModelBuilder) = 
+		modelBuilder += (path -> readValue(schema, reader))
+		
+     private def readValue(schema: TElement, reader: CellReader) = schema(path).read(reader) 
+  }
+
+  case object Gap extends Column {
+    override def read(reader: CellReader, schema : TElement, modelBuilder: ModelBuilder) =
+      reader.skip(1)
+  }
+  
+  class TReader(
+      mapping: Mapping, 
+      schema: TElement, 
+      width: Int = 10, 
+      heigth: Int = 10) {
+    def read(sheet: Sheet): Seq[Model] = 
+      sheet.rows.grouped(6).map { inputGroup =>
+        val modelBuilder = new ModelBuilder()
+        val reader = new ColumnOrientedReader(inputGroup)
+        
+        for (column <- mapping.columns)
+          column.read(reader, schema, modelBuilder)
+          
+        modelBuilder.build
+      }.toSeq
+  }
+  
+  
+    //==================TEST UTILS====================
 
   object WorkBookFactory {
 
@@ -119,115 +152,160 @@ class TestDriver extends FunSpec {
       sheet
     }
   }
+  
+  
+  
+  implicit def tModel2Reader(schema:TModel) = new {
+    def read(mapping: Mapping, sheet: Sheet) = new TReader(mapping, schema).read(sheet)
+  }
+
+  //==================TEST======================
+
+  describe("reader usage") {
+    it("should let read empty sheets using empty mappings ") {
+      val schema = TModel()
+      val sheet: Sheet = WorkBookFactory.makeEmptyDataItem
+      val mapping = Mapping()
+      val result : Seq[Model] = schema.read(mapping, sheet)
+      assert(result === Seq(Model()))
+    }
+    
+    it("should let read empty sheets using non-empty mappings ") {
+      val schema = TModel('foo -> TString)
+      val sheet: Sheet = WorkBookFactory.makeEmptyDataItem
+      val mapping = Mapping(Feature(Path('foo)))
+      val result = schema.read(mapping, sheet)
+      assert(result === Seq(Model('foo -> Value())))
+    }
+
+    it("should let read non-empty sheets using non-empty mappings ") {
+      val schema = TModel('foo -> TString)
+      val sheet: Sheet = WorkBookFactory.makeSingleDataItem("value", "calc", "comment", "not", "link")
+      val mapping = Mapping(Feature(Path('foo)))
+      val result = schema.read(mapping, sheet)
+      assert(result === Seq(Model('foo ->
+        Value(
+          Some("value"),
+          Some("calc"),
+          Some("comment"),
+          Some("not"),
+          Some("link")))))
+    }
+  }
+   
 
   describe("mapper creation") {
+
     it("should be able to create empty mappers") {
-      Mapper(Seq())
+      Mapping()
     }
 
-    it("should be able to create mappers with a single Integer Feature") {
-      Mapper(Seq(Feature('a, NumericType)))
-    }
-
-    it("should be able to create mappers with a single Integer Feature with default value") {
-      Mapper(Seq(Feature('a, WithDefault(NumericType, 1: BigDecimal))))
-    }
-
-    it("should be able to create mappers with mixed type Features") {
-      Mapper(
-        Seq(Feature('a, WithDefault(NumericType, 1: BigDecimal)),
-          Feature('a, StringType)))
-    }
-
-    it("should be able to describe gaps") {
-      Mapper(
-        Seq(
-          Feature('a, WithDefault(NumericType, 1: BigDecimal)),
-          Gap,
-          Gap,
-          Feature('a, StringType)))
-    }
-
-    it("should be able to describe enumurated Features") {
-      Mapper(Seq(Feature('a, EnumType(Seq("foo", "bar")))))
-    }
+//    it("should be able to create mappers with a single Integer Feature") {
+//      Mapping(Seq(Feature('a, NumericType)))
+//    }
+//
+//    it("should be able to create mappers with a single Integer Feature with default value") {
+//      Mapping(Seq(Feature('a, WithDefault(NumericType, 1: BigDecimal))))
+//    }
+//
+//    it("should be able to create mappers with mixed type Features") {
+//      Mapping(
+//        Seq(Feature('a, WithDefault(NumericType, 1: BigDecimal)),
+//          Feature('a, StringType)))
+//    }
+//
+//    it("should be able to describe gaps") {
+//      Mapping(
+//        Seq(
+//          Feature('a, WithDefault(NumericType, 1: BigDecimal)),
+//          Gap,
+//          Gap,
+//          Feature('a, StringType)))
+//    }
+//
+//    it("should be able to describe enumurated Features") {
+//      Mapping(Seq(Feature('a, EnumType(Seq("foo", "bar")))))
+//    }
 
   }
 
   describe("mapper for input parsing") {
     import WorkBookFactory._
-    it("should be able to parse valid files using an empty Feature definition") {
-      val sheet = makeSingleDataItem(15, "calc", "comment", "note", "link")
-      val items: Seq[DataItem] = Mapper(Seq(Gap, Feature('aFieldName, NumericType))).read(sheet)
-      assert(items(0)('aFieldName) === Value(Some(15.0), Some("calc"), Some("comment"), Some("note"), Some("link")))
-    }
-
-    it("should be able to parse valid string cells") {
-      val sheet = makeSingleDataItem("someValue", "calc", "comment", "note", "link")
-      val items: Seq[DataItem] = Mapper(Seq(Gap, Feature('someValidField, StringType))).read(sheet)
-      assert(items(0)('someValidField) === Value(Some("someValue"), Some("calc"), Some("comment"), Some("note"), Some("link")))
-    }
-
-    it("should be able to parse valid enum type fields") {
-      val sheet = makeSingleDataItem("someValue", "as", "as", "as", "as")
-      val items: Seq[DataItem] =
-        Mapper(
-          Seq(
-            Gap,
-            Feature('someValidField,
-              EnumType(Traversable("someKindOfValue", "someValue", "blah"))))).read(sheet)
-    }
-
-    it("should refuse invalid enum type fields") {
-      intercept[IllegalArgumentException] {
-        val sheet = makeSingleDataItem("invalidValue", "as", "as", "as", "as")
-        val items: Seq[DataItem] =
-          Mapper(Seq(
-            Gap,
-            Feature('someValidField,
-              EnumType(Traversable("someKindOfValue", "valid", "nana"))))).read(sheet)
-      }
-    }
-
-    it("should be able to take empty fields as valid enum type fields") {
-      val sheet = makeEmptyDataItem
-      Mapper(
-        Seq(
-          Gap,
-          Feature('someValidField,
-            EnumType(Traversable("someKindOfValue", "someValue", "blah"))))).read(sheet)
-    }
-
-    it("should be able to parse valid files using a single Feature definition ") {
-      Mapper(Seq(Feature('a, NumericType)))
-    }
+//    it("should be able to parse valid files using an empty Feature definition") {
+//      val sheet = makeSingleDataItem(15, "calc", "comment", "note", "link")
+//      val items: Seq[DataItem] = Mapping(Seq(Gap, Feature('aFieldName, NumericType))).read(sheet)
+//      assert(items(0)('aFieldName) === Value(Some(15.0), Some("calc"), Some("comment"), Some("note"), Some("link")))
+//    }
+//
+//    it("should be able to parse valid string cells") {
+//      val sheet = makeSingleDataItem("someValue", "calc", "comment", "note", "link")
+//      val items: Seq[DataItem] = Mapping(Seq(Gap, Feature('someValidField, StringType))).read(sheet)
+//      assert(items(0)('someValidField) === Value(Some("someValue"), Some("calc"), Some("comment"), Some("note"), Some("link")))
+//    }
+//
+//    it("should be able to parse valid enum type fields") {
+//      val sheet = makeSingleDataItem("someValue", "as", "as", "as", "as")
+//      val items: Seq[DataItem] =
+//        Mapping(
+//          Seq(
+//            Gap,
+//            Feature('someValidField,
+//              EnumType(Traversable("someKindOfValue", "someValue", "blah"))))).read(sheet)
+//    }
+//
+//    it("should refuse invalid enum type fields") {
+//      intercept[IllegalArgumentException] {
+//        val sheet = makeSingleDataItem("invalidValue", "as", "as", "as", "as")
+//        val items: Seq[DataItem] =
+//          Mapping(Seq(
+//            Gap,
+//            Feature('someValidField,
+//              EnumType(Traversable("someKindOfValue", "valid", "nana"))))).read(sheet)
+//      }
+//    }
+//
+//    it("should be able to take empty fields as valid enum type fields") {
+//      val sheet = makeEmptyDataItem
+//      Mapping(
+//        Seq(
+//          Gap,
+//          Feature('someValidField,
+//            EnumType(Traversable("someKindOfValue", "someValue", "blah"))))).read(sheet)
+//    }
+//
+//    it("should be able to parse valid files using a single Feature definition ") {
+//      Mapping(Seq(Feature('a, NumericType)))
+//    }
 
     it("should be able to convert blank cells to None") {
       val sheet = makeEmptyDataItem
-      val items: Seq[DataItem] =
-        Mapper(
-          Seq(
+      val schema = TModel('aField -> TString)
+      val mapping  = Mapping(
             Gap,
-            Feature('aField,
-              StringType))).read(sheet)
-      assert(items(0)('aField) === Value())
+            Feature(Path('aField)))
+      
+      val result = schema.read(mapping, sheet)
+      assert(result === Model('aField -> Value()))
+      
     }
 
-    it("should be able to convert blank cells to a given default value") {
-      val sheet = makeEmptyDataItem
-      val items: Seq[DataItem] =
-        Mapper(
-          Seq(
-            Gap,
-            Feature('aField,
-              WithDefault(StringType, "BLANK")))).read(sheet)
-      assert(items(0)('aField) === Value("BLANK"))
-    }
+//    it("should be able to convert blank cells to a given default value") {
+//	  val sheet = makeEmptyDataItem
+//      val schema = TModel('aField -> WithDefault(TString, "BLANK"))
+//      val mapping  =
+//        Mapping(
+//          Seq(
+//            Gap,
+//            Feature(Path('aField))))
+//      
+//      val result : Element = schema.read(mapping, sheet)
+//      assert(result === Model('aField -> Value("BLANK")))
+//    }
   }
 
   //  describe("mapper for output marshalling") {
   //    it("should write a single numeric value") {
-  //      val mapper = Mapper(Seq(Gap, Feature('aFieldName, NumericType)))
+  //      val mapper = Mapping(Seq(Gap, Feature('aFieldName, NumericType)))
   //      val sheetResult = mapper.write(Seq(Map('aFieldName -> Value(Some(15), Some("calc"), Some("comment"), Some("note"), Some("link")))))
   //      assert(15 === sheetResult.cellAt(0, 1).toString.toInt)
   //      assert("calc" === sheetResult.cellAt(1, 1).toString)
@@ -237,7 +315,7 @@ class TestDriver extends FunSpec {
   //    }
   //
   //    it("should write multiple values") {
-  //      val mapper = Mapper(Seq(Gap, Feature('numericField, NumericType), Feature('stringField, StringType)))
+  //      val mapper = Mapping(Seq(Gap, Feature('numericField, NumericType), Feature('stringField, StringType)))
   //      val sheetResult = mapper.write(
   //        Seq(
   //          Map('numericField -> Value(15),
@@ -247,13 +325,13 @@ class TestDriver extends FunSpec {
   //    }
   //
   //    it("should write blank cell for a numeric default value") {
-  //      val mapper = Mapper(Seq(Gap, Feature('numericField, WithDefault(NumericType, 0: BigDecimal))))
+  //      val mapper = Mapping(Seq(Gap, Feature('numericField, WithDefault(NumericType, 0: BigDecimal))))
   //      val sheetResult = mapper.write(Seq(Map('numericField -> Value(0))))
   //      assert(sheetResult.cellAt(0, 1).getCellType === Cell.CELL_TYPE_BLANK)
   //    }
   //
   //    it("should write blank cell for a string default value") {
-  //      val mapper = Mapper(Seq(Gap, Feature('stringField, WithDefault(StringType, "BLANK"))))
+  //      val mapper = Mapping(Seq(Gap, Feature('stringField, WithDefault(StringType, "BLANK"))))
   //      val sheetResult = mapper.write(Seq(Map('stringField -> Value("BLANK"))))
   //      assert(sheetResult.cellAt(0, 1).getCellType === Cell.CELL_TYPE_BLANK)
   //    }
