31a6c703f0ff79a86ba8168e8bc600c23d8807ba
Matias Corbanini <matiascorbanini@zauberlabs.com>
2013-05-03 19:40:10 -0300

    output generation updated with calculations

--- app/output/SpreadsheetWriter.scala
+++ app/output/SpreadsheetWriter.scala
@@ -10,9 +10,10 @@ import libt._
 import libt.spreadsheet._
 import libt.spreadsheet.util._
 import libt.spreadsheet.reader._
+import libt.reduction._
 
 object SpreadsheetWriter {
-
+  
   def write(out: OutputStream, companies: Seq[Model]): Unit = {
     FileManager.load("docs/external/EmptyOutputTemplate.xls") {
       x =>
@@ -23,8 +24,9 @@ object SpreadsheetWriter {
         }
     }
   }
+
   def write(out: Workbook, companies: Seq[Model]): Unit = {
-    val areas = Seq(ValueAreaLayout(Offset(0,0)), MetadataAreaLayout(Offset(0,0))).map(
+    val areas = Seq(ValueAreaLayout(Offset(6, 2)), MetadataAreaLayout(Offset(1, 0))).map(
       FlattedArea(
         PK(Path('ticker), Path('name), Path('disclosureFiscalYear)),
         PK(Path('lastName)),
@@ -32,16 +34,59 @@ object SpreadsheetWriter {
         TCompanyFiscalYear,
         _,
         Seq(
-          Feature('firstName),
+          //Exec Data
+          Gap,
           Feature('lastName),
           Feature('title),
+          Gap, //Leave Blank
           Feature('functionalMatches, 'primary),
-          Feature('functionalMatches, 'secondary),
-          Feature('functionalMatches, 'level),
-          Feature('functionalMatches, 'scope),
-          Feature('functionalMatches, 'bod),
           Feature('founder),
-          Feature('transitionPeriod))))
+          Gap, //TTDC Calculation
+          
+          //Cash Compensation
+          //Current Year
+          Gap,
+          Feature('cashCompensations, 'baseSalary),
+          Feature('cashCompensations, 'actualBonus),
+          Gap, //TTDC Calculation
+          Feature('cashCompensations, 'targetBonus),
+          Feature('cashCompensations, 'thresholdBonus),
+          Feature('cashCompensations, 'maxBonus),
+          //New 8-K Data
+          Feature('cashCompensations, 'nextFiscalYearData, 'baseSalary),
+          Feature('cashCompensations, 'nextFiscalYearData, 'targetBonus),
+          
+          //Equity Comp Value
+          //Options Value
+          Gap,
+          Calc(Sum(Path('optionGrants, *, 'value))), 
+          Calc(Sum(Path('optionGrants, *, 'number))), 
+          Calc(Average(Path('optionGrants, *, 'price))),
+          Gap, //Leave Blank
+          //Time Vest RS
+          Calc(Sum(Path('timeVestRS, *, 'value))), 
+          Calc(Sum(Path('timeVestRS, *, 'number))), 
+          Calc(Average(Path('timeVestRS, *, 'price))), 
+          //Perf RS
+          Calc(Sum(Path('performanceVestRS, *, 'targetValue))),
+          Calc(Sum(Path('performanceVestRS, *, 'targetNumber))), 
+          Calc(Average(Path('performanceVestRS, *, 'grantDatePrice))), 
+          //Perf Cash
+          Calc(Sum(Path('performanceCash, *, 'targetValue))),
+          
+          //Carried Interest
+          Gap,
+          Calc(SubstractAll(
+              Path('carriedInterest, 'ownedShares),
+              Path('beneficialOwnership),
+              Path('options),
+              Path('unvestedRestrictedStock),
+              Path('disclaimBeneficialOwnership))),
+          Feature('carriedInterest, 'outstandingEquityAwards, 'vestedOptions),
+          Feature('carriedInterest, 'outstandingEquityAwards, 'unvestedOptions),
+          Feature('carriedInterest, 'outstandingEquityAwards, 'timeVestRS),
+          Feature('carriedInterest, 'outstandingEquityAwards, 'perfVestRS)
+          )))
     WorkbookMapping(areas).write(companies, out)
   }
 
