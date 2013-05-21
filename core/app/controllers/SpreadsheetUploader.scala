bfcc8af2327eb8f1af53f705cd36e510d95f0b6f
Matias Corbanini <matiascorbanini@zauberlabs.com>
2013-05-21 11:53:43 -0300

    using same input name for files attached

--- core/app/controllers/SpreadsheetUploader.scala
+++ core/app/controllers/SpreadsheetUploader.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 trait SpreadsheetUploader { self: Controller =>
   def UploadSpreadsheetAction[A](block: FilePart[TemporaryFile] => Result) = Action(parse.multipartFormData) {
     request =>
-      request.body.file("datasets").map(block).getOrElse {
+      request.body.file("dataset").map(block).getOrElse {
         Redirect(routes.Application.companies).flashing("error" -> "Missing file")
       }
   }
