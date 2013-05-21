75dc2c841b8dd1956ef5d045b1b5cbd1404a0e00
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-05-20 21:57:27 -0300

    Extracting constant

--- core/app/libt/spreadsheet/LibtSizes.scala
+++ core/app/libt/spreadsheet/LibtSizes.scala
@@ -3,11 +3,18 @@ package libt.spreadsheet
 /**Sizes of the meta model, in terms of number of spreadsheet cells*/
 trait LibtSizes {
 
-  /**Size of a whole {{libt.Value}}*/
-  val ValueSize = MetadataSize + 1
-  /**Size of metadata fields of a {{libt.Value}}*/
-  val MetadataSize = 4
   /**Size of the titles seq of a {{libt.spreadsheet.Feature}}*/
   val TitlesSize = 2
+
+  /**Size of metadata fields of a {{libt.Value}}*/
+  val MetadataSize = 4
+  
+  /**Size of a whole {{libt.Value}}*/
+  val ValueSize = MetadataSize + 1
+  
+  /**Size of a whole {{libt.Value}}, plus a blank row*/
+  val ValueSizeWithSeparator = ValueSize + 1
+  
+  
 
 }
