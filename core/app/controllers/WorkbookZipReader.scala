12336826bffbba992e505dcd575ef59962a870cf
Franco Leonardo Bulgarelli <franco@zauberlabs.com>
2013-05-20 21:43:48 -0300

    Adding some scaladocs

--- core/app/controllers/WorkbookZipReader.scala
+++ core/app/controllers/WorkbookZipReader.scala
@@ -7,21 +7,25 @@ import scala.collection.JavaConversions._
 import libt.spreadsheet.reader._
 import libt._
 
+/**Trait that provides behavior for reading zipped spreadsheet sets */
 trait WorkbookZipReader[A] {
+  /**TODO semantics not clear*/
   val suffix: String
+  /**the reader used to parse each spreadsheet*/
   val reader : WorkbookReader[A]
 
+  /**answers a seq of file names and read results*/
   def readZipFileEntries(filePath: String) = {
     val file = new ZipFile(filePath)
     readZipFile(file, getValidEntries(file).toSeq)
   }
   
-  def readZipFile(file: ZipFile, entries: Seq[ZipEntry]) =
+  protected def readZipFile(file: ZipFile, entries: Seq[ZipEntry]) =
     entries
       .map { entry => (entry.getName(), reader.read(file.getInputStream(entry))) }
       .toSeq
 
-  def getValidEntries(file: ZipFile) =
+  protected def getValidEntries(file: ZipFile) =
     file
       .entries
       .map { entry => file.getEntry(entry.getName()) }
