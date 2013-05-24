8e32a62e8072db977915129af66c91816e3fdb63
Mauro Ponce <mgp1989@gmail.com>
2013-05-24 14:40:48 -0300

    Added seach result controller

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,15 +1,18 @@
 WindosorFrontend::Application.routes.draw do
+
   root to: "home#index"
-  get "dashboard/index"
 
   ActiveAdmin.routes(self)
-
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   devise_for :users
-  resources :groups, only: [:new, :index, :create] do
+  resources :groups, only: [:new, :create] do
     collection do
       get :tickers
     end
-  end  
+  end
+
+  get "dashboard/index"
+  post "search/results"
+
 end
