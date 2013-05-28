3b1c06dbcd0d10d73abdac9bac394aa7b82b6a47
Mauro Ponce <mgp1989@gmail.com>
2013-05-28 13:13:58 -0300

    Generating json query from params hash

--- webapp/app/controllers/search_controller.rb
+++ webapp/app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   before_filter :authenticate_user!  
   def results
+    query = params.except(:controller, :action, :authenticity_token, :utf8).to_json.gsub!(/\"/, '\'').gsub(/'(\d)\'/,' \1')
     binding.pry
+    # pegarle al server
   end
 end
