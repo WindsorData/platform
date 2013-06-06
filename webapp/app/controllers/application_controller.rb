ab80880120e43b77bd378c5489f51c3584538d48
Mauro Ponce <mgp1989@gmail.com>
2013-06-06 11:36:29 -0300

    Different home per user role

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :user_root_path # Available for views
 
   def after_sign_in_path_for(resource)
-    if resource.role == 'super'
-      users_path
-    elsif resource.role == 'admin'
-      single_file_upload_path # Change for upload file page
-    elsif resource.role == 'client'
-      quick_search_path      
-    end
+    user_root_path(resource)
   end
 
   def after_sign_out_path_for(resource_or_scope)
@@ -17,6 +12,17 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     render "#{Rails.root}/public/401"
+  end
+
+  def user_root_path(user)
+    case user.role
+    when 'super'
+      users_path
+    when 'admin'
+      single_file_upload_path
+    when 'client'
+      quick_search_path
+    end
   end
 
 end
