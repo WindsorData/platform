37a649af87982c1647f830c9b6532911f93ab1eb
Mauro Ponce <mgp1989@gmail.com>
2013-07-02 14:45:16 -0300

    Changed json query to fit new format

--- webapp/app/models/query_generator.rb
+++ webapp/app/models/query_generator.rb
@@ -9,35 +9,43 @@ class QueryGenerator
         a[:executives][i1] = Hash[executivesFilters: [([])]]
         params_hash[k1].each_with_index { |(k2, v2), i2|
           if params_hash[k1][k2].is_a? Hash
-            if params_hash[k1][k2]['type']
-              if params_hash[k1][k2]['gt'] && params_hash[k1][k2]['lt']
-                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [( [Hash[gt: params_hash[k1][k2]['gt'], lt: params_hash[k1][k2]['lt']]] )] ]
-              elsif params_hash[k1][k2]['gt']
-                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [( [Hash[gt: params_hash[k1][k2]['gt']]])]]
-              elsif params_hash[k1][k2]['lt']
-                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [( [Hash[lt: params_hash[k1][k2]['lt']]])]]
+            if !params_hash[k1][k2]['type'].blank?
+              if !params_hash[k1][k2]['gt'].blank? && !params_hash[k1][k2]['lt'].blank?
+                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [ Hash[operator: 'gt', value: params_hash[k1][k2]['gt']],
+                  Hash[operator: 'lt', value: params_hash[k1][k2]['lt']]] ]
+              elsif !params_hash[k1][k2]['gt'].blank?
+                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [ Hash[operator: 'gt', value: params_hash[k1][k2]['gt']]] ]
+              elsif !params_hash[k1][k2]['lt'].blank?
+                hash = Hash[key: k2 + "." + params_hash[k1][k2]['type'].downcase, operators: [ Hash[operator: 'lt', value: params_hash[k1][k2]['lt']]] ]
               end
             else
-              if params_hash[k1][k2]['gt'] && params_hash[k1][k2]['lt']
+              if !params_hash[k1][k2]['gt'].blank? && !params_hash[k1][k2]['lt'].blank?
                 hash = Hash[key: k2, operators: Array.new( [Hash[gt: params_hash[k1][k2]['gt'], lt: params_hash[k1][k2]['lt']]] ) ]
-              elsif params_hash[k1][k2]['gt']
+              elsif !params_hash[k1][k2]['gt'].blank?
                 hash = Hash[key: k2, operators: Array.new( [Hash[gt: params_hash[k1][k2]['gt']]])]
-              elsif params_hash[k1][k2]['lt']
+              elsif !params_hash[k1][k2]['lt'].blank?
                 hash = Hash[key: k2, operators: Array.new( [Hash[lt: params_hash[k1][k2]['lt']]])]
               end
             end     
           else
-            hash = Hash[key: k2, value: params_hash[k1][k2]]
+            hash = Hash[key: k2, value: params_hash[k1][k2]] unless params_hash[k1][k2].blank?
           end
-          a[:executives][i1][:executivesFilters][i2] = hash
+          a[:executives][i1][:executivesFilters][i2] = hash if hash
         }
       else # advanced search
         a[:advanced] << Hash[key: k1, values: params_hash[k1]]
       end
     }
+    
+    # Clear values
     a.keys.each{ |k|
       a.delete(k) if a[k].empty?
     }
-    json_query = a.to_json.gsub!(/\"/, '\'').gsub(/'(\d)\'/,' \1')
+    a[:executives][0][:executivesFilters].delete_if {|x| x.blank? }
+    a[:executives][0].delete(:executivesFilters) if a[:executives][0][:executivesFilters].blank?
+    a[:executives].delete_if {|x| x.blank?}
+    a.delete(:executives) if a[:executives].blank?
+
+    json_query = a.blank? ? "" : a.to_json.gsub!(/\"/, '\'').gsub(/'(\d)\'/,' \1')
   end
 end
