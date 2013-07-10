03c5bb0afe53272d7ae7f6ed3884a3da8ab5f2cc
Mauro Ponce <mgp1989@gmail.com>
2013-07-10 16:41:50 -0300

    Small changes in routes

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -2,7 +2,7 @@ WindosorFrontend::Application.routes.draw do
   root to: "home#index"
   devise_for :users
 
-  resources :users, except: [:show]
+  resources :users, except: [:show, :new]
   resources :companies
   resources :groups do
     collection do
@@ -10,11 +10,11 @@ WindosorFrontend::Application.routes.draw do
     end
   end
 
-  get "search/quick_search", as: :quick_search
-  get "search/full_search", as: :full_search
-  post "search/results"  
+  get "search/quick_search",  as: :quick_search
+  get "search/full_search",   as: :full_search
+  post "search/results",      as: :search_result
 
-  get   "files/upload", as: 'file_upload'
-  post  "files/send_file"
+  get   "files/upload",       as: :file_upload
+  post  "files/send_file",    as: :send_file
 
 end
