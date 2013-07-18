136e71b08e3448c2a3a573a26983ef5c723d6761
gastonprieto <gaston@zaubersoftware.com>
2013-07-18 17:52:11 -0300

    Fix upload log migration

--- webapp/db/migrate/20130709001605_create_upload_logs.rb
+++ webapp/db/migrate/20130709001605_create_upload_logs.rb
@@ -1,7 +1,7 @@
 class CreateUploadLogs < ActiveRecord::Migration
   def change
     create_table :upload_logs do |t|
-      t.string :user
+      t.integer :user_id
       t.text :message
       t.string :upload_type
 
