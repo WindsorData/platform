2f6780e9ebccd8137387c1ea8f562db91e5056c5
gastonprieto <gaston@zaubersoftware.com>
2013-07-26 18:05:33 -0300

    Filter upload logs by ticker

--- webapp/app/controllers/upload_log_controller.rb
+++ webapp/app/controllers/upload_log_controller.rb
@@ -8,12 +8,15 @@ class UploadLogController < ApplicationController
     user_id = params_hash['user'].to_i unless params_hash['user'].blank?
     since = params_hash['date_since'].try(:to_date).try(:beginning_of_day)
     to = params_hash['date_to'].try(:to_date).try(:end_of_day)
+    ticker = params_hash['ticker'] unless params_hash['ticker'].blank?
 
     @logs = UploadLog
               .with_user(user_id)
+              .with_ticker(ticker)
               .created_since(since)
               .created_to(to)
               .order('created_at desc').paginated(params)
     @users = User.order('email desc')
+    @tickers = Ticker.order('ticker desc')
   end
 end
