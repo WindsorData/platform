6ba12214f2cbe2de2aa06d9c2c06c21be9f01fe1
Mauro Ponce <mgp1989@gmail.com>
2013-07-29 14:14:10 -0300

    fix rebase problem

--- webapp/app/controllers/search_controller.rb
+++ webapp/app/controllers/search_controller.rb
@@ -1,27 +1,19 @@
 class SearchController < ApplicationController
-  before_filter :get_form_values, only: [:full_search, :recent_search]
+  before_filter :find_groups, only: [:quick_search, :full_search]
 
   def quick_search
     authorize!(:perform, :quick_search)
-    @groups = current_user.is_client? ? Group.by_company(current_user.company) : @groups = Group.all
-
-    # Get n recent searches
-    n = 5
-    if current_user.is_super?
-      @searches = Search.last_ordered_by_date(n)
-    elsif current_user.is_client?
-      @searches = Search.by_company(current_user.company, n)
-    end
-
   end
-  
+
   def full_search
     authorize!(:perform, :full_search)
+    @primary_roles = PrimaryRole.order("name asc")
+    @eq_comp_values = EquityCompensation.order("value asc")
+    @cash_comp_values = CashCompensation.order("value asc")
   end
 
   def results
     params_hash = params.except(:controller, :action, :authenticity_token, :utf8, :role_form)
-    Search.create(user: current_user, json_query: params_hash.to_json, company: current_user.company)
     json_query = QueryGenerator.json_query(params_hash)
     path = Rails.application.config.backend_host + Rails.application.config.post_query_path
 
@@ -40,19 +32,11 @@ class SearchController < ApplicationController
     tickers = Group.find(params[:group]).tickers.map(&:cusip)
     json_query = { range: 1, companies: tickers }.to_json.gsub(/(")(\d+)(")/, ' \2')
     perform_search(json_query, params[:report])
-    end
-  end
-
-  def recent_search
-    authorize!(:perform, :recent_search)
-    @params_hash = JSON.parse(Search.find(params[:id]).json_query)
   end
 
   private
-  def get_form_values
-    @primary_roles = PrimaryRole.order("name asc")
-    @eq_comp_values = EquityCompensation.order("value asc")
-    @cash_comp_values = CashCompensation.order("value asc")    
+  def find_groups
+    @groups = current_user.is_client? ? Group.by_company(current_user.company) : @groups = Group.all
   end
 
   def perform_search(json_query, report_type)
--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -62,6 +62,14 @@ ActiveRecord::Schema.define(:version => 20130726172458) do
     t.string   "type"
   end
 
+  create_table "searches", :force => true do |t|
+    t.integer  "user_id"
+    t.text     "json_query"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "company_id"
+  end
+
   create_table "tickers", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -72,7 +80,6 @@ ActiveRecord::Schema.define(:version => 20130726172458) do
 
   create_table "upload_logs", :force => true do |t|
     t.integer  "user_id"
-    t.text     "message"
     t.string   "upload_type"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
