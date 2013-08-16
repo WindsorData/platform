4e42c3763bf5d27dfff4ea8ac04534b1627b7593
Ubuntu <ubuntu@ip-10-254-21-54.us-west-2.compute.internal>
2013-08-16 22:12:51 +0000

    Fix merge
    Parameter db in report

--- webapp/db/seeds.rb
+++ webapp/db/seeds.rb
@@ -1,27 +1,4 @@
-zauber = Company.create!(name: "Zauber", address: "Costa Rica 5546, Palermo, Bs As", contact_email: "contact@zauber.com")
-Company.create!(name: "Rojo Oriente", address: "Uriarte 1284, Palermo, Bs As")
-
 # Users
 User.create!(email: "super@windsor.com", password: "123456", password_confirmation: "123456", role: "super")
 User.create!(email: "admin@windsor.com", password: "123456", password_confirmation: "123456", role: "admin")
-client_from_zauber = User.create!(email: "client@windsor.com", password: "123456", password_confirmation: "123456", role: "client", company: zauber)
 
-# Tickers
-ticker_aapl = Ticker.create!(ticker: 'appl', name: 'Apple Inc', cusip: "123abc")
-ticker_fb = Ticker.create!(ticker: 'fb', name: 'Facebook Inc', cusip: "456def")
-
-# Groups
-group1 = Group.new(name: 'group with company', company: client_from_zauber.company)
-group1.tickers << ticker_aapl
-group1.save!
-
-group2 =  Group.new(name: 'group without company')
-group2.tickers << ticker_aapl
-group2.tickers << ticker_fb
-group2.save!
-
-# Roles
-PrimaryRole.create!([{name: "CTO"},{name: "CFO"}])
-
-# Company Peers
-CompanyPeer.create!([{name: "Coompany Ticker 1", ticker: "CT1"}, {name: "BIIB", ticker: "BIIB"}])
