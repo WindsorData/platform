0b1e1587fc0c27be1d24989d7b55d1638e32e43f
gastonprieto <gaston@zaubersoftware.com>
2013-09-03 15:21:03 -0300

    FIx db parameter

--- core/app/controllers/Api.scala
+++ core/app/controllers/Api.scala
@@ -95,14 +95,14 @@ object Api extends Controller with SpreadsheetDownloader {
     request.body.asJson.map { json =>
       val range = (json \ "range").as[Int]
       val companies = (json \ "companies").as[Seq[String]]
-      createSpreadsheetResult(writer, companies, range)(ExecutivesDb) match {
+      createSpreadsheetResult(writer, companies, range)(db) match {
         case Some(response) => response
         case None => NotFound("not found companies")
       }
     }.getOrElse(BadRequest("invalid json"))
   }
 
-  def top5Report = companiesReport(StandardTop5Writer,ExecutivesDb)
+  def top5Report = companiesReport(StandardTop5Writer, ExecutivesDb)
   def bodReport = companiesReport(BodWriter, BodDb)
   def fullReport = companiesReport(FullTop5Writer, ExecutivesDb)
 
