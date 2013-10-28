ae0a8bf36f78b33d68d7f590d14f965a349f2fe0
Mauro Ponce <mgp1989@gmail.com>
2013-10-28 14:24:02 -0300

    Removed ticker list switch

--- webapp/app/assets/javascripts/company_peers.js
+++ webapp/app/assets/javascripts/company_peers.js
@@ -9,13 +9,6 @@ $(document).ready(function() {
     tokenLimit: 1 // remove to select more than one
   });  
 
-  $("#toggleFormLink").click(function(){ 
-    $("#single_ticker_form").toggleClass("hidden");
-    $("#ticker_list_form").toggleClass("hidden");
-    var text = $("#toggleFormLink").text();
-    text == "Switch to Tickers List" ?  $("#toggleFormLink").text("Switch to Single Ticker") : $("#toggleFormLink").text("Switch to Tickers List");
-  });
-
   $("input[name=secondary_type]").click(function(e){
     $("#normalized").toggleClass("hidden");
     $("#unnormalized").toggleClass("hidden");
