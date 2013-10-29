cbfa745345ea08e2cc0dc886de695652cf8cfadf
Mauro Ponce <mgp1989@gmail.com>
2013-10-29 16:13:19 -0300

    Adding loading gif for upload log results

--- webapp/app/assets/javascripts/upload_log.js
+++ webapp/app/assets/javascripts/upload_log.js
@@ -1,14 +1,17 @@
 $(function() {
-    $("#date_since")
-        .datepicker({
-            onClose: function(selectedDate) {
-                $("#date_to").datepicker("option", "minDate", selectedDate);
-            }
-        });
-    $("#date_to")
-        .datepicker({
-            onClose: function(selectedDate) {
-                $("#date_since").datepicker("option", "maxDate", selectedDate);
-            }
-        });
+  $("#date_since")
+    .datepicker({
+      onClose: function(selectedDate) {
+        $("#date_to").datepicker("option", "minDate", selectedDate);
+      }
+    });
+  $("#date_to")
+  .datepicker({
+    onClose: function(selectedDate) {
+      $("#date_since").datepicker("option", "maxDate", selectedDate);
+    }
+  });
+  $("#search-button").click(function(){
+    $("#log-container").html('<p id="loading-gif" style="text-align: center;"><img src="/assets/ajax-loader.gif" /></p>');
+  });
 });
