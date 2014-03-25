53870182a35ae9b0c02f72d1286b3a71d8ac9c4f
Matias Corbanini <matiascorbanini@zauberlabs.com>
2014-03-25 12:15:18 -0300

    change password ability for every user

--- webapp/app/models/ability.rb
+++ webapp/app/models/ability.rb
@@ -20,6 +20,9 @@ class Ability
     when 'admin'
       can   :upload, :file
       can   :perform, :full_search
+      can [:update_account, :edit_account], User do |u|
+        u == user
+      end
       can   :manage, Company
       can   :audit, :upload_log
       can   [:read, :create], Group
@@ -35,6 +38,9 @@ class Ability
       can   :destroy, Group do |g|
         g.user == user
       end
+      can [:update_account, :edit_account], User do |u|
+        u == user
+      end
       can   :read, Group do |g|
         g.company == user.company && user.company
       end
@@ -44,6 +50,10 @@ class Ability
       can   :perform, :quick_search
       can   :perform, :full_search
       can   :perform, :recent_search
+    when 'client_peer_peer'
+      can [:update_account, :edit_account], User do |u|
+        u == user
+      end
     end
   end
 end
