64c2ddf8d9671fc1fa064c0b4d039b9d3f42e790
Matias Corbanini <matiascorbanini@zauberlabs.com>
2014-03-26 19:17:30 -0300

    pst format helper

--- webapp/app/controllers/upload_log_controller.rb
+++ webapp/app/controllers/upload_log_controller.rb
@@ -13,14 +13,15 @@ class UploadLogController < ApplicationController
     user_id = params_hash['user'].to_i unless params_hash['user'].blank?
     since = params_hash['date_since'].try(:to_date).try(:beginning_of_day)
     to = params_hash['date_to'].try(:to_date).try(:end_of_day)
-    ticker = params_hash['upload-log-ticker'] unless params_hash['upload-log-ticker'].blank?
 
+    ticker = params_hash['upload-log-ticker'] unless params_hash['upload-log-ticker'].blank?
     @logs = UploadLog
               .with_user(user_id)
               .with_ticker(ticker)
               .created_since(since)
               .created_to(to)
               .order('created_at desc').paginated(params)
+
     respond_to do |format|
       format.js { render 'display_search'}
     end
@@ -31,5 +32,11 @@ class UploadLogController < ApplicationController
     respond_to do |format|
       format.xls { render 'upload_log_file'}
     end
+  end
+
+  def to_PST_date(date)
+    Time.parse(date.to_s)
+      .in_time_zone("Pacific Time (US & Canada)")
+      .strftime('%B %e at %l:%M %p')
   end
 end
